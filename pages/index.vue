e9ee1285dc284bcdae44c64bd2651f5a5a29b97d
muhammadnurdin <muhammad.nurdin@sekolahmu.co.id>
2025-06-30 17:12:18 +0700

    add landing page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96">
+      <div class="max-w-md">
+        <h1 class="text-5xl font-bold">
+          Travel Log
+        </h1>
+        <p class="py-6">
+          Keep track of your journeys, memories, and adventures in one place. Start logging your travels today!
+        </p>
+        <button class="btn btn-accent">
+          Sign in With Github <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
