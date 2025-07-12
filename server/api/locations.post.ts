0020ece6538062db42196c56fb4178b9ee648b13
iampo <muhammadnurdin460@gmail.com>
2025-07-12 11:49:01 +0700

    :sparkles: slug and name unique validation (#62)
    Co-authored-by: muhammadnurdin <muhammad.nurdin@sekolahmu.co.id>

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,6 +1,12 @@
+import type { DrizzleError } from "drizzle-orm";
+
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
+
 import db from "~/lib/db";
 import { InsertLocation, location } from "~/lib/db/schema";
 
+const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
     return sendError(event, createError({
@@ -31,10 +37,30 @@ export default defineEventHandler(async (event) => {
     }));
   }
 
-  const [created] = await db.insert(location).values({
-    ...result.data,
-    slug: result.data.name.replaceAll(" ", "-").toLowerCase(),
-    userId: event.context.user.id,
-  }).returning();
-  return created;
+  // Generate a unique slug by combining slugified name and current datetime in milliseconds
+  const slug = slugify(`${result.data.name}-${nanoid()}-${Date.now()}`);
+  try {
+    const [created] = await db.insert(location).values({
+      ...result.data,
+      slug,
+      userId: event.context.user.id,
+    }).returning();
+    return created;
+  }
+  catch (e) {
+    const error = e as DrizzleError;
+    if (error.message === "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: location.slug") {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "Slug must be unique (the location name is used to generate the slug).",
+      }));
+    }
+    if (error.message === "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: location.name, location.user_id") {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "A location with this name already exists!",
+      }));
+    }
+    throw error;
+  }
 });
