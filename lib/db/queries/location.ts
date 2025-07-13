2fe90b1c129a7a128f68e83dc3c9ac859987cd15
iampo <muhammadnurdin460@gmail.com>
2025-07-13 11:36:09 +0700

    :sparkles: list locations (#64)
    Co-authored-by: muhammadnurdin <muhammad.nurdin@sekolahmu.co.id>

--- lib/db/queries/location.ts
+++ lib/db/queries/location.ts
@@ -1,9 +1,16 @@
+import { eq } from "drizzle-orm";
+
 import type { InsertLocation } from "~/lib/db/schema";
 
 import { location } from "~/lib/db/schema";
 
 import db from "..";
 
+export async function findLocations(userId: number) {
+  return db.query.location.findMany({
+    where: eq(location.userId, userId),
+  });
+}
 export async function insertLocation(insertable: InsertLocation, slug: string, userId: number) {
   const [created] = await db.insert(location).values({
     ...insertable,
