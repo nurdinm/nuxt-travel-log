f0c1e38346170e0c99980ba37847fdaa4427ac26
muhammadnurdin <muhammad.nurdin@sekolahmu.co.id>
2025-07-13 11:34:59 +0700

    :sparkles: list locations

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -5,15 +5,10 @@ import slugify from "slug";
 
 import { insertLocation } from "~/lib/db/queries/location";
 import { InsertLocation } from "~/lib/db/schema";
+import defineAuthenticatedEventHandler from "~/utils/define-authenticated-event-handler";
 
 const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
-export default defineEventHandler(async (event) => {
-  if (!event.context.user) {
-    return sendError(event, createError({
-      statusCode: 401,
-      statusMessage: "Unauthorized",
-    }));
-  }
+export default defineAuthenticatedEventHandler(async (event) => {
   const result = await readValidatedBody(event, InsertLocation.safeParse);
   if (!result.success) {
     const statusMessage = result
